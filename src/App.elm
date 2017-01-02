ca1adce1605caecb88768ef27f86600e51d331f3
Bob Whitelock <bob.whitelock1@gmail.com>
2017-01-02 01:38:35 +0000

    Initial work for drawing grid

--- src/App.elm
+++ src/App.elm
@@ -1,18 +1,22 @@
 module App exposing (..)
 
-import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
 type alias Model =
-    { message : String
-    , logo : String
+    { livingCells : List Cell
     }
 
 
+type alias Cell =
+    ( Int, Int )
+
+
 init : String -> ( Model, Cmd Msg )
-init path =
-    ( { message = "Your Elm App is working!", logo = path }, Cmd.none )
+init _ =
+    ( { livingCells = [ ( 1, 1 ), ( 1, 3 ) ] }, Cmd.none )
 
 
 type Msg
@@ -26,10 +30,80 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src model.logo ] []
-        , div [] [ text model.message ]
+    svg
+        [ width "1000"
+        , height "1000"
+        , viewBox "0 0 500 500"
         ]
+        (List.concat
+            [ gridLines
+            , gridCells model.livingCells
+            ]
+        )
+
+
+gridLines : List (Svg Msg)
+gridLines =
+    let
+        lineRange =
+            List.range 1 11
+
+        linesUsing =
+            \lineFunction ->
+                List.map (\n -> lineFunction (10 * n)) lineRange
+    in
+        List.concat
+            [ linesUsing verticalLineAt
+            , linesUsing horizontalLineAt
+            ]
+
+
+
+-- TODO: get rid of implicit hard-coding of cell/border size numbers.
+
+
+verticalLineAt : Int -> Svg Msg
+verticalLineAt xCoord =
+    line ( xCoord, 10 ) ( xCoord, 110 )
+
+
+horizontalLineAt : Int -> Svg Msg
+horizontalLineAt yCoord =
+    line ( 10, yCoord ) ( 110, yCoord )
+
+
+line : ( Int, Int ) -> ( Int, Int ) -> Svg Msg
+line ( xStart, yStart ) ( xEnd, yEnd ) =
+    Svg.line
+        [ x1 (toString xStart)
+        , y1 (toString yStart)
+        , x2 (toString xEnd)
+        , y2 (toString yEnd)
+        , strokeWidth "1"
+        , stroke "black"
+        ]
+        []
+
+
+gridCells : List Cell -> List (Svg Msg)
+gridCells cells =
+    let
+        drawCellRect =
+            \( x, y ) ->
+                rect ( 10 + (10 * x), 10 + (10 * y) )
+    in
+        List.map drawCellRect cells
+
+
+rect : ( Int, Int ) -> Svg Msg
+rect ( rectX, rectY ) =
+    Svg.rect
+        [ x (toString rectX)
+        , y (toString rectY)
+        , width "10"
+        , height "10"
+        ]
+        []
 
 
 subscriptions : Model -> Sub Msg
