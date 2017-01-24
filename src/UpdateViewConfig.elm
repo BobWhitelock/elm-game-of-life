1644f4a6ac8444101dde2eb51a1b21a5f41a9e7e
Bob Whitelock <bob.whitelock1@gmail.com>
2017-01-24 22:43:56 +0000

    Keep centre of grid the same after zooming in/out

--- src/UpdateViewConfig.elm
+++ src/UpdateViewConfig.elm
@@ -7,10 +7,32 @@ import ZoomLevel exposing (ZoomLevel)
 zoom : (ZoomLevel -> ZoomLevel) -> ViewConfig -> ViewConfig
 zoom changeZoom viewConfig =
     let
-        newViewConfig =
+        newZoomLevel =
+            changeZoom viewConfig.zoomLevel
+
+        zoomedViewConfig =
             { viewConfig
-                | zoomLevel =
-                    changeZoom viewConfig.zoomLevel
+                | zoomLevel = newZoomLevel
+            }
+
+        visibleCellsChange =
+            ViewConfig.visibleCells zoomedViewConfig - ViewConfig.visibleCells viewConfig
+
+        topLeftShift =
+            -- After a zoom, the new `topLeft` Cell will be the old `topLeft`
+            -- shifted by half the change in the number of visible Cells - this
+            -- way the centre of the view will remain the same after any zoom.
+            visibleCellsChange // 2
+
+        ( x, y ) =
+            viewConfig.topLeft
+
+        newTopLeft =
+            ( x - topLeftShift, y - topLeftShift )
+
+        newViewConfig =
+            { zoomedViewConfig
+                | topLeft = newTopLeft
             }
     in
         newViewConfig
--- tests/UpdateViewConfigTests.elm
+++ tests/UpdateViewConfigTests.elm
@@ -3,7 +3,7 @@ module UpdateViewConfigTests exposing (all)
 import Test exposing (..)
 import Expect
 import UpdateViewConfig exposing (..)
-import ViewConfig
+import ViewConfig exposing (ViewConfig)
 import ZoomLevel
 
 
@@ -14,9 +14,6 @@ all =
             [ test "new zoomLevel is current changed by changeZoom" <|
                 \() ->
                     let
-                        viewConfig =
-                            ViewConfig.defaultConfig
-
                         changeZoom =
                             ZoomLevel.zoomIn
 
@@ -24,5 +21,35 @@ all =
                             zoom changeZoom viewConfig |> .zoomLevel
                     in
                         Expect.equal (changeZoom viewConfig.zoomLevel) newZoomLevel
+            , test "topLeft Cell is shifted inwards correctly when zoom in" <|
+                \() ->
+                    let
+                        newTopLeftCell =
+                            zoom ZoomLevel.zoomIn viewConfig |> .topLeft
+                    in
+                        Expect.equal ( -1, -11 ) newTopLeftCell
+            , test "topLeft Cell is shifted outwards correctly when zoom out" <|
+                \() ->
+                    let
+                        newTopLeftCell =
+                            zoom ZoomLevel.zoomOut viewConfig |> .topLeft
+                    in
+                        Expect.equal ( -28, -38 ) newTopLeftCell
             ]
         ]
+
+
+viewConfig : ViewConfig
+viewConfig =
+    let
+        default =
+            ViewConfig.defaultConfig
+    in
+        { default
+            | topLeft =
+                ( -10, -20 )
+                -- Values carefully chosen so `visibleCells` == 36 for nice
+                -- round numbers in tests.
+            , borderSize = 0
+            , svgSize = 360
+        }
